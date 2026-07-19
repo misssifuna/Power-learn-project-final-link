1a8b1a11adab9f8e4d3c0b2c79ca18de1a2f2867
misssifuna <sifunaebenezer@gmail.com>
2026-07-19 16:03:45 +0300

    Migrate Bakulutu project into PLP reposiroty

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,14 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
+    // Redirect TanStack Start's bundled server entry to src/server.ts
     server: { entry: "server" },
+
+    // Attempt static prerendering
+    prerender: {
+      enabled: true,
+      crawlLinks: true,
+      autoStaticPathsDiscovery: true,
+    },
   },
 });
